6ee42a1594e322b5ca6c948a93baddb0e15a3a4a
Leeeydia <leeeydia@gmail.com>
2026-01-14 16:42:17 +0900

    DB연결

---
+++ src/main/java/com/KoreaIT/java/AM_jsp/SecSql.java
@@ -0,0 +1,22 @@
+package com.KoreaIT.java.AM_jsp;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/article/list")
+public class SecSql extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.getWriter()
+        .append("HelloWorld!!!!!!!!!!!!!")
+        .append(request.getContextPath());
+
+	}
+
+}
